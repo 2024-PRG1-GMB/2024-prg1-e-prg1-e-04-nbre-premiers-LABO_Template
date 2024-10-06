efa5c9b26b3f1533a0a8ed5dc353700910b6ad7f
cedrickdev <cedrickv.tchindaf@hes-so.ch>
2024-10-06 21:08:37 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    cout << "Entrer une valeur [2-1000] : ";
+    unsigned int limite;
+    cin >> limite;
+
+    if (limite < 2 || limite > 1000) {
+        cout << "Entrer une valeur [2-1000] : " << endl;
+        cin >> limite;
+    }
+
+    cout << "Voici la liste des nombres premiers" << limite << endl;
+    for (int i = 2; i <= limite; i++) {
+        // 2 et 3 sont premiers
+        if (i == 2 ) {
+            cout << i << " ";
+        }
+
+        // Vérifie si le nombre est pair
+        if (i % 2 == 0) {
+            continue; // Les nombres pairs ne sont pas premiers (sauf 2)
+        }
+
+        bool est_premier = true; // Utilisé seulement pour le contrôle de la boucle
+        for (int j = 3; j * j <= i; j += 2) {
+            if (i % j == 0) {
+                est_premier = false; // Trouvé un diviseur
+            }
+        }
+
+        // Afficher le nombre s'il est premier
+        if (est_premier) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    return EXIT_SUCCESS;
+}
+
+
+
